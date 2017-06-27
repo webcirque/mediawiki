5c9c37a0e9a9ccfab8ef7a64159bd3ce535ab2c4
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-06-27 09:08:45 +0800

    Update ui_translate_zh-CN.js

--- gamepedia/ui_translate_zh-CN.js
+++ gamepedia/ui_translate_zh-CN.js
@@ -20,7 +20,7 @@ function _uitranslation() {
     output = output.replace(">hist<",">历史<");
     output = output.replace(')">',')">于');
     output = output.replace(" days","天");
-    output = output.replace("a day","一天");
+    output = output.replace("a day","1天");
     output = output.replace(" ago","前");
     output = output.replace(" hours","个小时");
     output = output.replace("an hour","1个小时");
