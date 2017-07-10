1c58b3fe77fb3af83018fe153e7718b5000f54ef
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-07-10 19:56:18 +0800

    Create adremover.dev.js

---
+++ gamepedia/adremover.dev.js
@@ -0,0 +1,24 @@
+// Yes, this is a advertisement remover.
+function permanentBlock() {
+ if (document.getElementsByClassName('ad-placement ad-main-med-rect-footer')[0] !== undefined) {
+ document.getElementsByClassName('ad-placement ad-main-med-rect-footer')[0].innerHTML="";
+ document.getElementsByClassName('ad-placement ad-main-med-rect-footer')[0].style.display="none";}
+ if (document.getElementById("atflb") !== null) {
+document.getElementById("atflb").style = 'position: absolute; top:-800px; ';
+document.getElementById("atflb").innerHTML = '<center style="color: red; font-size: 25px; font-family: "Verdana"; ">Element blocked.</center>';}
+ if (document.getElementById("btflb") !== null) {
+document.getElementById("btflb").style = 'position: absolute; top:-800px; ';}
+ if (document.getElementById("siderail") !== null) {
+document.getElementById("siderail").style = 'position: absolute; top:-800px; height: 203px; ';
+document.getElementById("siderail").innerHTML = '&nbsp#;';}
+ if (document.getElementById("p-sharing") !== null) {
+document.getElementById("p-sharing").innerHTML = '<span style="color: #ffffff; font-family: "Verdana"; ">WikiAdBlock</span>';}
+document.getElementById("bodyContent").style.width="100%";
+ if (document.getElementById("siderail") !== null) {
+siderail = document.getElementById("siderail");
+document.getElementById("siderail").style = 'position: absolute; top:-2000px; left:-2000px; ';
+document.getElementById("siderail").innerHTML = '';}}
+ if (clockwork == undefined) {
+ var clockwork = setInterval("permanentBlock()",20);}
+ 
+ // Here for twitch remover and video ad remover.
