f6cee49504fd64f8447cbc25ac267a0fda2989e0
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-07-12 00:44:49 +0800

    Update adremover.dev.js

--- gamepedia/adremover.dev.js
+++ gamepedia/adremover.dev.js
@@ -1,5 +1,10 @@
 // A new definition to AdRemover.
-
+blockList = ["atflb","btflb","siderail","ad-placement ad-main-med-rect-footer","lotNbButton","lotBeWrapper","p-sharing"];
+dn = 0; _d = document;
+while(dn<blockList.length) {
+  
+ dn++;
+}
 
 // When script is loaded.
 console.warn('Gamepedia AdRemover is initialized.');
