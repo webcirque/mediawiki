2869e88c51439d89947236cedbbf3766238fbe8b
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-07-12 01:58:34 +0800

    Update adremover.dev.js

--- gamepedia/adremover.dev.js
+++ gamepedia/adremover.dev.js
@@ -1,9 +1,10 @@
 // A new definition to AdRemover.
 blockList = ["atflb","btflb","siderail","lotNbButton","lotBeWrapper","p-sharing","class:ad-placement","cdm-zone-03","class:ad-main-med-rect-footer"];
+function permBlock() {
 dn = 0; _d = document; confirmedStyle="display:none;opacity:0;width:0px;height:0px;position:absolute;top:-2000px;";
 while(dn<blockList.length) {
  if (blockList[dn].search("class:") > -1 ) {
-  console.log("Blocking element by "+blockList[dn]+".");
+  if (debug==true) {console.log("Blocking element by "+blockList[dn]+".");}
   ele = _d.getElementsByClassName(blockList[dn].replace("class:",""))[0];
   if (ele !== null) {
   ele.innerHTML = "";
@@ -12,7 +13,7 @@ while(dn<blockList.length) {
   else {console.warn("Element ID ["+blockList[dn]+"] not found, ignoring it.")}
  }
  else {
-  console.log("Blocking element by id:"+blockList[dn]+".");
+  if (debug==true) {console.log("Blocking element by id:"+blockList[dn]+".");}
   ele = _d.getElementById(blockList[dn]);
   if (ele !== null) {
   ele.innerHTML = "";
@@ -21,6 +22,8 @@ while(dn<blockList.length) {
  }
  dn++;
 }
+}
+setInterval("permBlock()",125)
 
 // When script is loaded.
 console.warn('Gamepedia AdRemover version DR3 has finished executing.');
