1fc6edc6f37bfcb812e8562de62434cdfc27e12a
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2017-12-15 23:01:09 +0800

    Update ui_translate_zh-CN.js

--- gamepedia/ui_translate_zh-CN.js
+++ gamepedia/ui_translate_zh-CN.js
@@ -38,6 +38,8 @@ function _uitranslation() {
     output = output.replace(" minutes","分钟");
     output = output.replace("a minute","1分钟");
     output = output.replace("less than ","不到");
+    output = output.replace("Created","创建了");
+    output = output.replace("Edited","编辑了");
     output = output.replace('</a> (<a','</a> （<a');
     output = output.replace('</a>) <time','</a>） <time');
     output = output.replace("于于","于");
