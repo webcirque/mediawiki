e5db7ffaa671184ed5a0af315fabde4ee78b52ef
Alex Daring <42095187+Angrydog001@users.noreply.github.com>
2019-02-07 13:18:40 +0800

    Update ui_translate_zh-CN.js

--- gamepedia/ui_translate_zh-CN.js
+++ gamepedia/ui_translate_zh-CN.js
@@ -1,29 +1,10 @@
-//MediaWiki Information
-function _mw() {
- pageInfo = window.location.search.replace("?","");
- if (window.location.pathname.search("index.php") > -1) {
- _mw.pagename = _wsgetItem(pageInfo,"title","&","=");
- _mw.namespace = _mw.pagename.split(":")[0];}
-else {
- _mw.namespace = window.location.pathname.replace("/","").split(":")[0];
- _mw.pagename = window.location.pathname.replace("/","");}
- if (_mw.namespace == _mw.pagename){
-  _mw.namespace = "(default)";
- }
-}
-_mw();
-console.warn("MediaWiki Information Module initialized.");
-
-function _uitranslation() {
- document.getElementById("Friends").innerHTML = "好友";
- document.getElementById("Achievements").onnerHTML = "成就";
- if (_mw.namespace == "UserProfile") {
-  actvs = document.getElementsByClassName("activity section")[0];
-  if (actvs) {
-   donum = 0;
-   actnum = actvs.getElementsByTagName("li").length;
-   while (donum <actnum) {
-    output = actvs.getElementsByTagName("li")[donum].innerHTML;
+$(function() {
+ if (mw.config.get("wgUserLanguage").indexOf("zh") == -1) return;
+ $("#Friends").html("好友");
+ $("#Achievements").html("成就");
+ if (mw.config.get("wgNamespaceNumber") != 202) return;
+  $(".activity.section:first li").each(function() {
+    output = $(this).html();
     output = output.replace(">diff<",">差异<");
     output = output.replace(">hist<",">历史<");
     output = output.replace(')">',')">于');
@@ -45,24 +26,6 @@ function _uitranslation() {
     output = output.replace('</a> (<a','</a> （<a');
     output = output.replace('</a>) <time','</a>） <time');
     output = output.replace("于于","于");
-    actvs.getElementsByTagName("li")[donum].innerHTML = output;
-    donum ++;
-   }
-   donum = undefined;
-   actnum = undefined;
-   output = undefined;
-  }
- }
-}
-console.warn("UI translation initialized.");
-
-function _uitranslation_stop() {
- clearInterval(_uitranslation.loopId);
- console.warn("UI translation stopped.");}
-function _uitranslation_run() {
- _uitranslation.loopId = setInterval("_uitranslation();",100);
-console.warn("UI translation is running...");}
-_uitranslation_run();
-console.warn("UI translation is fully initialized.");
-
-_uitranslation.loaded = true;
+    $(this).html(output);
+  });
+});
